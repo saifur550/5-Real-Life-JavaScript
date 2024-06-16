eb600a6b749fc3c0bccfff83f9f3ce116b79f1e9
saifur550 <srstap550@gmail.com>
2024-06-16 18:42:06 +0600

    css style

---
+++ insta-love-btn/main.js
@@ -0,0 +1,15 @@
+
+const mainContent= document.querySelector('.container')
+const icon = document.querySelector('i')
+
+
+let count = 0
+mainContent.addEventListener('dblclick', function(){
+    icon.style.transform= 'translate(-50%,-50%) scale(1)'
+   
+})
+
+
+setTimeout(() => {
+    
+}, 2000);
