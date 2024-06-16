809d0ec236d7a5c9477be43e6f934567625a653d
saifur550 <srstap550@gmail.com>
2024-06-16 19:38:34 +0600

    project done

--- insta-love-btn/main.js
+++ insta-love-btn/main.js
@@ -3,13 +3,20 @@ const mainContent= document.querySelector('.container')
 const icon = document.querySelector('i')
 
 
-let count = 0
 mainContent.addEventListener('dblclick', function(){
-    icon.style.transform= 'translate(-50%,-50%) scale(1)'
+    icon.style.transform= 'translate(-50%,-50%) scale(1)';
+     icon.style.opacity='0.8'
+
+     setTimeout(() => {
+          icon.style.transform='0'
+     }, 1000);
+     
+    setTimeout(() => {
+        icon.style.transform= 'translate(-50%,-50%) scale(0)';
+        icon.style.color='red'
+        
+    }, 2000);
    
 })
 
 
-setTimeout(() => {
-    
-}, 2000);
